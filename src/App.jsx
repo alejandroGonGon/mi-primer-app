3f0adc6b93d8cc3982b69bc18734b6b6aab41f1b
alejandrogon1418 <alejandro.gonzalez@anima.edu.uy>
2021-05-31 09:13:27 -0300

    archivos

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
+        <p>
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
+        </p>
+        <p>{process.env.REACT_APP_NOT_SECRET_CODE}</p>
+        <p>{process.env.NODE_ENV}</p>
+        <p>{process.env.BROWSERLIST}</p>
+        <a
+          className={styles.AppLink}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
