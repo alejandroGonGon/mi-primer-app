18eada4f4f8122636bca33761b6ce676b57c1871
alejandrogon1418 <alejandro.gonzalez@anima.edu.uy>
2021-08-06 12:31:15 -0300

    changing all to functional component

--- src/components/peliculas.jsx
+++ src/components/peliculas.jsx
@@ -1,107 +1,100 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable react/no-unused-state */
-/* eslint-disable no-unused-vars */
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { getPeliculas } from '../servicios/Services';
 
-class Pelicula extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      idPel: '',
-      nombre: '',
-      url: '',
-      peliculas: [],
-
+function Pelicula() {
+  const [peliculas, setPeliculas] = useState([]);
+  const [idPel, setIdPel] = useState('');
+  const [nombre, setNombrePeli] = useState('');
+  const [url, setUrlPeli] = useState('');
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const bringPeliculas = async () => {
+      try {
+        const responseJson = await getPeliculas();
+        setPeliculas(responseJson.data);
+      } catch (error) {
+        // setError
+      } finally {
+        setLoading(false);
+      }
     };
-  }
+    bringPeliculas();
+  }, []);
 
-  async componentDidMount() {
-    const responseJson = await getPeliculas();
-    this.setState({ peliculas: responseJson.data });
-  }
+  const toStateNombre = (event) => {
+    setNombrePeli(event.target.value);
+  };
 
-  postPelicula = async () => {
-    const { nombre, url } = this.state;
-    const peli = { name: nombre, img: url };
+  const toStateUrl = (event) => {
+    setUrlPeli(event.target.value);
+  };
+
+  const postPelicula = async () => {
+    const peli = { nombre: `${nombre}`, img: url };
     try {
       await axios.post('http://anima-api.localhost/api/pelicula', peli);
     } catch (error) {
       // setError
     }
-  }
+  };
 
-  toState = (event) => {
-    if (event.target.id === 'tituloPelicula') {
-      this.setState({ nombre: event.target.value });
-    } else {
-      this.setState({ url: event.target.value });
-    }
-  }
-
-  renderPeliculas = () => {
-    const { peliculas } = this.state;
-    return (
-      <div>
-        {peliculas.map((pelicula) => (
-          <>
-            <div className="cardPelicula">
-              <div className="imgagePelicula">
-                <img src={pelicula.img} alt="" />
-              </div>
-              <div className="nombrePelicula">
-                <h2>{pelicula.nombre}</h2>
-              </div>
-            </div>
-          </>
-        ))}
-      </div>
-    );
-  }
-
-    getPelicula = async (event) => {
-      const e = event.target.value;
-      this.setState({ idPel: event.target.value });
-    };
-
-    renderUnaPeli = () => {
-      const { peliculas, idPel } = this.state;
-      if (idPel > 0) {
-        const pelicula = peliculas.filter(({ idPelicula }) => idPelicula === parseFloat(idPel));
-        return (
+  const renderPeliculas = () => (
+    <div>
+      {peliculas.map((pelicula) => (
+        <>
           <div className="cardPelicula">
             <div className="imgagePelicula">
-              <img src={pelicula[0].img} alt="" />
+              <img src={pelicula.img} alt="" />
             </div>
             <div className="nombrePelicula">
               <h2>{pelicula.nombre}</h2>
             </div>
           </div>
-        );
-      }
-      return console.log('a');
-    }
+        </>
+      ))}
+    </div>
+  );
 
-    render() {
+  const getPelicula = async (event) => {
+    const e = event.target.value;
+    setIdPel(e);
+  };
+
+  const renderUnaPeli = () => {
+    if (idPel > 0) {
+      const pelicula = peliculas.filter(({ idPelicula }) => idPelicula === parseFloat(idPel));
       return (
-        <div className="">
-          <div className="formCrearPelicula">
-            <input id="tituloPelicula" type="text" placeholder="Ingrese una pelicula" onChange={this.toState} />
-            <input id="urlPelicula" type="text" placeholder="Ingrese la url de la imagen" onChange={this.toState} />
-            <input id="submit-pelicula" type="button" value="submit" onClick={this.postPelicula} />
+        <div className="cardPelicula">
+          <div className="imgagePelicula">
+            <img src={pelicula[0].img} alt="" />
           </div>
-          <div className="verPeliculas">
-            { this.renderPeliculas() }
-          </div>
-          <div className="onePelicula">
-            <input type="text" id="searchPelicula" placeholder="id de pelicula" onChange={this.getPelicula} />
-            <input type="button" value="buscar" />
-            {this.renderUnaPeli()}
+          <div className="nombrePelicula">
+            <h2>{pelicula[0].nombre}</h2>
           </div>
         </div>
       );
     }
+    return console.log('Todavia Nada');
+  };
+  if (loading) return <p>loading peliculas...</p>;
+  return (
+    <div className="">
+      <div className="formCrearPelicula">
+        <input id="tituloPelicula" type="text" placeholder="Ingrese una pelicula" onChange={toStateNombre} />
+        <input id="urlPelicula" type="text" placeholder="Ingrese la url de la imagen" onChange={toStateUrl} />
+        <input id="submit-pelicula" type="button" value="submit" onClick={postPelicula} />
+      </div>
+      <div className="verPeliculas">
+        {renderPeliculas()}
+      </div>
+      <div className="onePelicula">
+        <input type="text" id="searchPelicula" placeholder="id de pelicula" onChange={getPelicula} />
+        {renderUnaPeli()}
+      </div>
+    </div>
+  );
 }
+
 export default Pelicula;
