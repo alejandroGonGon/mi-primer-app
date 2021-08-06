8a34465d77133aadb6bd60fed086ce9585f12b44
alejandrogon1418 <alejandro.gonzalez@anima.edu.uy>
2021-08-05 21:14:04 -0300

    todo de pelicukas

--- src/components/peliculas.jsx
+++ src/components/peliculas.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-unused-vars */
 
 import React from 'react';
 import axios from 'axios';
@@ -7,11 +9,12 @@ import { getPeliculas } from '../servicios/Services';
 class Pelicula extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
+      idPel: '',
       nombre: '',
       url: '',
       peliculas: [],
+
     };
   }
 
@@ -24,7 +27,7 @@ class Pelicula extends React.Component {
     const { nombre, url } = this.state;
     const peli = { name: nombre, img: url };
     try {
-      await axios.post('http://localhost/phpMailer-ejercicio/code/apiPeliculas.php/', peli);
+      await axios.post('http://anima-api.localhost/api/pelicula', peli);
     } catch (error) {
       // setError
     }
@@ -57,26 +60,29 @@ class Pelicula extends React.Component {
       </div>
     );
   }
-  /*
-    return (
-      <div className="cardPelicula">
-        <div className="imgagePelicula">
-          <img src={pelicula.img} alt="" />
-        </div>
-        <div className="nombrePelicula">
-          <h2>{pelicula.nombre}</h2>
-        </div>
-      </div>
-    );
-    */
 
     getPelicula = async (event) => {
       const e = event.target.value;
-      // console.log(e);
-      const { peliculas } = this.state;
-      const pelicula = peliculas.filter(({ idPelicula }) => idPelicula === e);
-      console.log(pelicula);
+      this.setState({ idPel: event.target.value });
     };
+
+    renderUnaPeli = () => {
+      const { peliculas, idPel } = this.state;
+      if (idPel > 0) {
+        const pelicula = peliculas.filter(({ idPelicula }) => idPelicula === parseFloat(idPel));
+        return (
+          <div className="cardPelicula">
+            <div className="imgagePelicula">
+              <img src={pelicula[0].img} alt="" />
+            </div>
+            <div className="nombrePelicula">
+              <h2>{pelicula.nombre}</h2>
+            </div>
+          </div>
+        );
+      }
+      return console.log('a');
+    }
 
     render() {
       return (
@@ -90,9 +96,9 @@ class Pelicula extends React.Component {
             { this.renderPeliculas() }
           </div>
           <div className="onePelicula">
-            <input type="text" id="searchPelicula" placeholder="nombre de pelicula" onChange={this.getPelicula} />
+            <input type="text" id="searchPelicula" placeholder="id de pelicula" onChange={this.getPelicula} />
             <input type="button" value="buscar" />
-
+            {this.renderUnaPeli()}
           </div>
         </div>
       );
